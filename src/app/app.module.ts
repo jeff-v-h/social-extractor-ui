6977f1b1207d33de47f3aa9fc4bad7575848d6b6
Jeffrey Huang <jeffvh@outlook.com>
2020-01-08 15:27:50 +0000

    add auth service and interceptors for token headers and errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,28 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpErrorHandler } from "./services/http-error-handler.service";
 import { MessageService } from "./services/message.service";
+import { JwtInterceptor } from "./helpers/jwt.interceptor";
+import { AuthErrorInterceptor } from "./helpers/auth-error.interceptor";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { LocationStrategy, PathLocationStrategy } from "@angular/common";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [HttpErrorHandler, MessageService],
+  declarations: [AppComponent, NavBarComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatSnackBarModule
+  ],
+  providers: [
+    HttpErrorHandler,
+    MessageService,
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthErrorInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
